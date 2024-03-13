77e5f03825e79708f765621f7c50b0b31f348880
Harivan-T <harivantahir@gmail.com>
2024-03-14 01:14:41 +0300

    completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
+  return true;
+}
 /* 
   Add your pseudocode here
 */
@@ -12,14 +19,32 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
 
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("lalal"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+}
 module.exports = isPalindrome;
